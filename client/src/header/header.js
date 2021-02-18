613a87593d974d21dfc6b88ad1243b91da711c14
OlgaBelogurova <obelogurova2@gmail.com>
2021-02-17 22:01:17 -0800

    Add header

---
+++ client/src/header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+    return (
+        <header className="app-header">
+            <h1>My App Title</h1>
+        </header>
+    )
+}
+
+export default Header;
