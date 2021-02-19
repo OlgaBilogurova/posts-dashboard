a884904f83ba44dc8cb23950522aba4bf41d42e0
OlgaBelogurova <obelogurova2@gmail.com>
2021-02-18 21:09:43 -0800

    Create constants for popup buttons, messages

---
+++ client/src/utils/constants.js
@@ -0,0 +1,11 @@
+export const ButtonName = {
+  CREATE_POST: 'Create New Post',
+  EDIT_POST: 'Edit Post',
+  DELETE_POST: 'Delete Post'
+}
+
+export const PopupMessages = {
+  CREATE_POST_MSG: 'New post successfully added!',
+  EDIT_POST_MSG: 'Post successfully edited!',
+  DELETE_POST_MSG: 'Post successfully deleted!'
+}
